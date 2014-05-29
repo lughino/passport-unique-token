519a256f751cd2022251e87f915694235d904263
Luca Pau <luca.pau82@gmail.com>
2014-05-29 17:58:49 +0200

    initial commit

---
+++ tests/strategy.fields.test.js
@@ -0,0 +1,83 @@
+/* global describe, it, expect, before */
+/* jshint expr: true */
+
+var chai = require('chai')
+  , Strategy = require('../lib/strategy');
+
+
+describe('Strategy', function() {
+    
+  describe('handling a request with valid credentials in body using custom field names', function() {
+    var strategy = new Strategy({ tokenField: 'sdvoihweui3498d9vhwoufhi8h' }, function(token, done) {
+      if (token == 'sdvoihweui3498d9vhwoufhi8h') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      return done(null, false);
+    });
+    
+    var user,
+        info;
+    
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body.userid = 'sdvoihweui3498d9vhwoufhi8h';
+        })
+        .authenticate();
+    });
+    
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+    
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+  
+  describe('handling a request with valid credentials in body using custom field names with object notation', function() {
+    var strategy = new Strategy({ tokenField: 'token[sdvoihweui3498d9vhwoufhi8h]' }, function(username, password, done) {
+      if (token == 'sdvoihweui3498d9vhwoufhi8h') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      return done(null, false);
+    });
+    
+    var user
+      , info;
+    
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body.user = {};
+          req.body.user.token = 'sdvoihweui3498d9vhwoufhi8h';
+        })
+        .authenticate();
+    });
+    
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+    
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+  
+});
