b36bc6d0637e86d7c2240d7c322e08be0eaf810a
Luis Rangel <luis.rangel@collect.ai>
2017-04-24 17:19:09 +0200

    Support case sensitive option

--- tests/strategy.fields.test.js
+++ tests/strategy.fields.test.js
@@ -42,6 +42,42 @@ describe('Strategy', function() {
     });
   });
 
+  describe('handling a request with valid credentials in body using case sensitive custom field name', function() {
+    var strategy = new Strategy({tokenField: 'uniqToken', caseSensitive: true}, function(token, done) {
+      if (token === 'sdvoihweui3498d9vhwoufhi8h') {
+        return done(null, {id: '1234'}, {scope: 'read'});
+      }
+      return done(null, false);
+    });
+
+    var user,
+      info;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body.uniqToken = 'sdvoihweui3498d9vhwoufhi8h';
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+
   describe('handling a request with valid credentials in body using custom field names with object notation', function() {
     var strategy = new Strategy({tokenField: 'token[unique]'}, function(token, done) {
       if (token === 'sdvoihweui3498d9vhwoufhi8h') {
