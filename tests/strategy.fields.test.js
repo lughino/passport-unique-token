ae34fc30d76fc88abc442ba579174ea26817be9b
Luis Rangel <xlarsx@gmail.com>
2017-06-11 14:49:55 +0200

    (feedback) adding failing test

--- tests/strategy.fields.test.js
+++ tests/strategy.fields.test.js
@@ -42,39 +42,69 @@ describe('Strategy', function() {
     });
   });
 
-  describe('handling a request with valid credentials in body using case sensitive custom field name', function() {
-    var strategy = new Strategy({tokenField: 'uniqToken', caseSensitive: true}, function(token, done) {
-      if (token === 'sdvoihweui3498d9vhwoufhi8h') {
-        return done(null, {id: '1234'}, {scope: 'read'});
-      }
-      return done(null, false);
+  describe('using case sensitive custom field name', function () {    
+    describe('handling a request with valid credentials in body', function() {
+       var strategy = new Strategy({tokenField: 'uniqToken', caseSensitive: true}, function(token, done) {
+        if (token === 'sdvoihweui3498d9vhwoufhi8h') {
+          return done(null, {id: '1234'}, {scope: 'read'});
+        }
+        return done(null, false);
+      });
+      
+      var user,
+        info;
+
+      before(function(done) {
+        chai.passport(strategy)
+          .success(function(u, i) {
+            user = u;
+            info = i;
+            done();
+          })
+          .req(function(req) {
+            req.body = {};
+            req.body.uniqToken = 'sdvoihweui3498d9vhwoufhi8h';
+          })
+          .authenticate();
+      });
+
+      it('should supply user', function() {
+        expect(user).to.be.an.object;
+        expect(user.id).to.equal('1234');
+      });
+
+      it('should supply info', function() {
+        expect(info).to.be.an.object;
+        expect(info.scope).to.equal('read');
+      });
     });
 
-    var user,
-      info;
+    describe('handling a request with invalid credentials', function() {
+      var strategy = new Strategy({tokenField: 'uniqToken', caseSensitive: true}, function(token, done) {
+        throw new Error('should not be called');
+      });
 
-    before(function(done) {
-      chai.passport(strategy)
-        .success(function(u, i) {
-          user = u;
-          info = i;
-          done();
-        })
-        .req(function(req) {
-          req.body = {};
-          req.body.uniqToken = 'sdvoihweui3498d9vhwoufhi8h';
-        })
-        .authenticate();
-    });
+      var info, status;
 
-    it('should supply user', function() {
-      expect(user).to.be.an.object;
-      expect(user.id).to.equal('1234');
-    });
+      before(function(done) {
+        chai.passport(strategy)
+          .fail(function(i, s) {
+            info = i;
+            status = s;
+            done();
+          })
+          .req(function(req) {
+            req.body = {};
+            req.body.uniqtoken = 'sdvoihweui3498d9vhwoufhi8h';
+          })
+          .authenticate();
+      });
 
-    it('should supply info', function() {
-      expect(info).to.be.an.object;
-      expect(info.scope).to.equal('read');
+      it('should fail with info and status', function() {
+        expect(info).to.be.an.object;
+        expect(info.message).to.equal('Missing credentials');
+        expect(status).to.equal(400);
+      });
     });
   });
 
