9423b757e4c67034be1c86fac275e5d71e752b47
Luca Pau <luca@giffgaff.co.uk>
2019-07-28 15:29:05 +0100

    chore(version): add auto bump package.json [skip ci]

--- release.config.js
+++ release.config.js
@@ -2,4 +2,11 @@ module.exports = {
   release: {
     branch: 'master',
   },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
 };
